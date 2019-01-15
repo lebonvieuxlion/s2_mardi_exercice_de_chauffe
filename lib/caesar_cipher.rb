92b72fa54ef5e6f23b49316ebee3772746091b03
lebonvieuxlion <debaugelion@gmail.com>
2019-01-15 20:28:23 +0100

    méthode 1 exercice fait

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,3 +1,26 @@
-def hello
-	return "Hello!"
+def letter (letter, key)
+
+	case
+
+		when letter.class == String
+
+				number_for_letter =  letter.ord
+				theoretical_ascii = number_for_letter + key 
+
+				if theoretical_ascii.between?(65, 90) || theoretical_ascii.between?(97, 122)
+
+					return theoretical_ascii.to_i.chr
+
+				else 
+
+					return (theoretical_ascii-26).to_i.chr
+
+				end
+
+		else return "Sorry it's not a letter"
+
+	end
+
 end
+
+puts letter("k", 3)
--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,7 +1,17 @@
 require_relative '../lib/caesar_cipher'
 
-describe "the hello function" do
-  it "says hello" do
-    expect(hello).to eq("Hello!")
+describe "the letter function" do
+
+  it "display another letter according to the key you entered" do
+    expect(letter("A", 3)).to eq("D")
+    expect(letter("Z", 3)).to eq("C")
+    expect(letter("a", 3)).to eq("d")
+    expect(letter("z", 3)).to eq("c")
   end
+
+    it "if the parameter entered is not a letter it displays an error message" do
+    expect(letter(1, 3)).to eq("Sorry it's not a letter")
+  end
+
+
 end
