d5e7af5e1e4c5d23110085d4d20ca3a507936070
lebonvieuxlion <debaugelion@gmail.com>
2019-01-16 00:06:03 +0100

    exercice 2 réalisé

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,26 +1,47 @@
-def letter (letter, key)
+def letter_cipher (letter, key)
 
 	case
 
-		when letter.class == String
+		when letter.class == String 													#vérifie en premier lieu que la lettre donnée est bien un string
 
-				number_for_letter =  letter.ord
-				theoretical_ascii = number_for_letter + key 
+				number_for_letter =  letter.ord 									    #obtention du numéro ASCII correspondant pour la lettre		
+				theoretical_ascii = number_for_letter + key 							#calcul du numéro théorique ASCII que pourrait avoir la lettre décalée
 
-				if theoretical_ascii.between?(65, 90) || theoretical_ascii.between?(97, 122)
+				if theoretical_ascii.between?(65, 90) || theoretical_ascii.between?(97, 122) 	#vérification que le numéro ASCII ne dépasse pas Z ou z
 
-					return theoretical_ascii.to_i.chr
-
+					return theoretical_ascii.to_i.chr 										    #si le numéro ASCII ne dépasse pas Z ou z on le garde
+																								#et on le transforme
 				else 
 
-					return (theoretical_ascii-26).to_i.chr
+					return (theoretical_ascii-26).to_i.chr  						#si il dépasse z ou Z on lui retire 26 pour obtenir son vrai numéro
 
 				end
 
-		else return "Sorry it's not a letter"
+		else return false															#si la lettre n'est pas un string on renvoie faux
 
 	end
 
 end
 
-puts letter("k", 3)
+
+
+
+
+def string_cipher (string, key)
+
+array_letter_from_string = string.split(//)								#met chaque élément de la string dans un array
+
+array_letter_from_string = array_letter_from_string.map  do |letter|	#transforme le array en réalisant une action sur la string 
+
+		if letter.match(/[a-zA-z]/)										#vérifie que pour chaque élément du array c'est une lettre en majuscule ou minuscule
+			letter = letter_cipher(letter, key)							#utilisation de la méthode letter_cipher pour transformer la lettre à l'aide de la clef
+		else 
+			letter 														#si ce n'est pas une lettre on renvoie l'élément du array normalement
+		end
+	end
+
+array_letter_from_string.join("")										#on joint le array sans espace car ces derniers n'ont pas été modifié
+
+end
+
+puts string_cipher("What a string!", 5)
--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,17 +1,25 @@
 require_relative '../lib/caesar_cipher'
 
-describe "the letter function" do
+describe "the letter cipher function" do
 
   it "display another letter according to the key you entered" do
-    expect(letter("A", 3)).to eq("D")
-    expect(letter("Z", 3)).to eq("C")
-    expect(letter("a", 3)).to eq("d")
-    expect(letter("z", 3)).to eq("c")
+    expect(letter_cipher("A", 3)).to eq("D")
+    expect(letter_cipher("Z", 3)).to eq("C")
+    expect(letter_cipher("a", 3)).to eq("d")
+    expect(letter_cipher("z", 3)).to eq("c")
   end
 
     it "if the parameter entered is not a letter it displays an error message" do
-    expect(letter(1, 3)).to eq("Sorry it's not a letter")
+    expect(letter_cipher(1, 3)).to eq(false)
   end
 
+
+end
+
+describe "the string caesar cipher function" do
+
+	it "transform each letter of a string into another letter according to a key" do
+		expect(string_cipher("What a string!", 5)).to eq("Bmfy f xywnsl!")
+	end
 
 end
